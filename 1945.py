cd3d32c34e3d4321c8c30bf5c717a05d24b0103d
gittgi <gittgi768@gmail.com>
2022-07-27 23:37:38 +0900

    1945 주석 변경

--- 1945.py
+++ 1945.py
@@ -2,15 +2,14 @@ case = int(input())
 
 for i in range(case):
     num = int(input())
-    divide_list = [2,3,5,7,11]
-    count_list = []
-    for j in divide_list:
+    divide_list = [2,3,5,7,11] # 인수분해 인수 목록
+    count_list = []            # 각 수마다 인수분해 한 횟수 담는 리스트
+    for j in divide_list:      # 2부터 차례대로
         count = 0
-        while num % j == 0:
-            num /= j
-            count += 1
-        count_list.append(count)
+        while num % j == 0:    # 나눴을때 나누어 떨어지는 상황이라면,
+						count += 1         # 카운트를 올리고
+            num /= j           # 나눈 후의 값으로 다시 반복 -> 나누어 떨어지지 않을때까지
+        count_list.append(count) # 리스트에 순서대로 담기
 
     print(f'#{i+1}',end=' ')
-    print(*count_list)
-
+    print(*count_list)         # 이거 너무 좋아요!
