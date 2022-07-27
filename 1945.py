db5632dc4486ecc2af4bd3901f04008790b2dfff
gittgi <gittgi768@gmail.com>
2022-07-27 23:39:50 +0900

    1945 들여쓰기 수정

--- 1945.py
+++ 1945.py
@@ -7,9 +7,9 @@ for i in range(case):
     for j in divide_list:      # 2부터 차례대로
         count = 0
         while num % j == 0:    # 나눴을때 나누어 떨어지는 상황이라면,
-						count += 1         # 카운트를 올리고
+            count += 1         # 카운트를 올리고
             num /= j           # 나눈 후의 값으로 다시 반복 -> 나누어 떨어지지 않을때까지
-        count_list.append(count) # 리스트에 순서대로 담기
+            count_list.append(count) # 리스트에 순서대로 담기
 
     print(f'#{i+1}',end=' ')
     print(*count_list)         # 이거 너무 좋아요!
