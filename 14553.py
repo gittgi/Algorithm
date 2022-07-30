160eeff5d459231bdb97e17e8eb64a7be46d4105
gittgi <gittgi768@gmail.com>
2022-07-30 15:35:45 +0900

    14553

--- 14553.py
+++ 14553.py
@@ -1,27 +1,14 @@
 test_case = int(input())
-
-
-for i in range(test_case):
-    n = int(input())
-    
-    player = list(map(int, input().split()))
-    
-    count = sum(player) // 2   
-
-    print(f'#{i + 1} {count}')
-
-'''
-여기는 어렵게 푼 버전 (메모리 초과 됨)
 for i in range(test_case):
     n = int(input())
     
     player = list(map(int, input().split()))
     count = 0
-    while sum(player) > 1:
+    player.sort(reverse=True)
+    while player[1] > 0:
+        player[0] -= 1
+        player[1] -= 1
+        count += 1
         player.sort(reverse=True)
-        count += player[1]
-        player[0] -= player[1]
-        player.pop(1) 
 
     print(f'#{i + 1} {count}')
-'''
